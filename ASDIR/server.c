deb4190b9caa7d1df42bbc24208c78e36d7f3cbb
Parece <90103455+MiguelParece@users.noreply.github.com>
2023-12-05 23:20:51 +0000

    server things

--- ASDIR/server.c
+++ ASDIR/server.c
@@ -1,5 +1,7 @@
 // server side code
+
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -38,6 +40,20 @@
 #include <sys/types.h>
 #include <sys/sendfile.h>
 #include <sys/mman.h>
+
+// structs
+typedef struct
+{
+    int auction_bid_id; // auction id
+    int bid_ammount;    // bid ammount
+} BID;
+
+typedef struct
+{
+    int no_bids;  // Number of bids in the list
+    BID bids[50]; // Assuming a maximum of 50 bids, adjust as needed
+} BIDLIST;
+
 // handle users
 int createUserDir(char *UID)
 {
@@ -50,6 +66,8 @@ int createUserDir(char *UID)
     snprintf(userDir, sizeof(userDir), "USERS/%s/HOSTED", UID);
     mkdir(userDir, 0777);
     snprintf(userDir, sizeof(userDir), "USERS/%s/BIDDED", UID);
+    mkdir(userDir, 0777);
+
     return 1;
 }
 
@@ -134,7 +152,6 @@ int erasePassword(char *UID)
 }
 
 // handle auctions
-
 int CreateAUCTIONDir(int AID)
 {
     char AID_dirname[15];
@@ -173,9 +190,195 @@ int CreateAUCTIONDir(int AID)
     return 1;
 }
 
+// create auction
+int StartAuction(int *AID, char *UID, char *nameDescription, char *fileName, char *startValue, char *timeactive)
+{
+    // create the start file
+    char startName[35];
+    snprintf(startName, sizeof(startName), "AUCTIONS/%03d/Start_%03d.txt", AID, AID);
+    FILE *fp;
+    fp = fopen(startName, "w");
+    if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        return 0;
+    }
+    // write in the start file UID name asset fname start value timeactive start datetime start fulltime
+    fprintf(fp, "%s %s %s %s %s \n", UID, nameDescription, fileName, startValue, timeactive);
+}
+
+int MakeBid(int AID, int UID, int bid_ammount)
+{
+    char AID_dirname[15];
+    char BIDS_dirname[20];
+    char UID_dirname[15];
+    char BID_filename[25];
+    int ret;
+
+    // Check if AID is within a valid range (1 to 999)
+    if (AID < 1 || AID > 999)
+    {
+        printf("Auction ID is invalid\n");
+        return 0;
+    }
+
+    // Create the directory path for the auction
+    sprintf(AID_dirname, "AUCTIONS/%03d", AID);
+
+    // Check if the auction directory exists
+    if (access(AID_dirname, F_OK) == -1)
+    {
+        printf("Auction directory does not exist\n");
+        return 0;
+    }
+
+    // Check if bid_ammount is valid
+    BIDLIST list;
+    int n = GetBidList(AID, &list);
+    if (n == 0)
+    {
+        printf("0 bids in this auction\n");
+        return 0;
+    }
+    int lastBid = list.bids[0].bid_ammount;
+
+    if (bid_ammount <= lastBid)
+    {
+        printf("Bid ammount is invalid\n");
+        return 0;
+    }
+    // Create the directory path for the bids within the auction directory
+    sprintf(BIDS_dirname, "AUCTIONS/%03d/BIDS", AID);
+
+    // Check if the bids directory exists
+    if (access(BIDS_dirname, F_OK) == -1)
+    {
+        printf("Bids directory does not exist\n");
+        return 0;
+    }
+
+    // Check if UID is within a valid range (1 to 999999)
+    if (UID < 1 || UID > 999999)
+    {
+        printf("User ID is invalid\n");
+        return 0;
+    }
+
+    sprintf(UID_dirname, "USERS/%06d", UID);
+
+    // Check if the user directory exists
+    if (access(UID_dirname, F_OK) == -1)
+    {
+        printf("User directory does not exist\n");
+        return 0;
+    }
+    // Create the full path to the bid file
+    sprintf(BID_filename, "AUCTIONS/%03d/BIDS/%06d.txt", AID, bid_ammount);
+
+    // Attempt to create the bid file
+    ret = creat(BID_filename, 0700);
+
+    // Check if the bid file creation was unsuccessful
+    if (ret == -1)
+    {
+        printf("Bid file creation on the server Dir side was unsuccessful\n");
+        return 0;
+    }
+
+    // add the bid to the user directory
+    sprintf(BID_filename, "USERS/%06d/BIDDED/%03d.txt", UID, AID);
+    ret = creat(BID_filename, 0700);
+    if (ret == -1)
+    {
+        printf("Bid file creation on the user Dir side was unsuccessful\n");
+        return 0;
+    }
+    // Return 1 to indicate successful creation of the bid file
+    return 1;
+}
+
+// Fazer um initializer ?
+int GetBidList(int AID, BIDLIST *list)
+{
+    struct dirent **filelist;
+    int entries, nbids, len;
+    char dirname[20];
+    char pathname[32];
+
+    // Create the directory path for the bids within the auction directory
+    sprintf(dirname, "AUCTIONS/%03d/BIDS/", AID);
+
+    // Scan the directory and get the list of files
+    entries = scandir(dirname, &filelist, 0, alphasort);
+
+    // Check if there are no entries in the directory
+    if (entries <= 0)
+        return 0;
+
+    nbids = 0;
+    list->no_bids = 0;
+
+    // Iterate through the directory entries
+    while (entries--)
+    {
+        len = strlen(filelist[entries]->d_name);
+
+        // Discard entries with length 10 (presumably, skip '.' and '..')
+        if (len == 10)
+        {
+            // Create the full path to the bid file
+            sprintf(pathname, "AUCTIONS/%03d/BIDS/%s", AID, filelist[entries]->d_name);
+
+            // Load the bid information from the file and increment the bid count
+            if (LoadBid(pathname, list))
+                ++nbids;
+        }
+
+        // Free memory allocated for the dirent structure
+        free(filelist[entries]);
+
+        // Break if the bid count reaches 50
+        if (nbids == 50)
+            break;
+    }
+
+    // Free memory allocated for the file list
+    free(filelist);
+
+    // Return the number of bids loaded
+    return nbids;
+}
+
+int LoadBid(const char *filename, BIDLIST *list)
+{
+    if (list->no_bids == 50)
+    {
+        printf("Bid list is full\n");
+        return 0;
+    }
+    sscanf(filename, "AUCTIONS/%d/BIDS/%d", &list->bids[list->no_bids].auction_bid_id, &list->bids[list->no_bids].bid_ammount);
+    list->no_bids++;
+    return 1;
+}
+
 int main()
 {
+    time_t fulltime;        // Declare a variable to store the current time in seconds.
+    struct tm *currenttime; // Declare a pointer to a structure to store the broken-down time.
+
+    char timestr[20]; // Declare an array to store the formatted time as a string.
+
+    time(&fulltime); // Get the current time in seconds since 1970.
+
+    currenttime = gmtime(&fulltime);
+    printf("UTC date and time: %s", asctime(currenttime));
+
     printf("Hello, World!\n");
-    CreateAUCTIONDir(322);
+    BIDLIST list;
+    list.no_bids = 0;
+    createUserDir("000001");
+    CreateLogin("000001");
+    CreatePassword("000001", "123456");
+    MakeBid(322, 000001, 8000);
     return 0;
 }
